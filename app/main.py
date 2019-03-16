b901343398379f853c3334bc501707e111eafa72
Quark98 <datoendeladze@hotmail.com>
2019-03-16 14:48:13 +0000

    add button and logo to app.

--- app/main.py
+++ app/main.py
@@ -17,9 +17,34 @@ for env_variable in sys.argv[1:]:
     except(IndexError) as e:  # Passed value has no equals
         pass
 
+
 import kivy
-import kivy.app as a
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+import numpy as np
 kivy.require('1.10.1')
+
+class InitialScreen(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(InitialScreen, self).__init__(**kwargs)
+        self.rows = 2
+        self.add_widget(Image(source='Phrijj.png'))
+        self.add_widget(Button(text='Enter'))
+
+
+
+
+
+class PhrijjApp(App):
+    def build(self):
+        return InitialScreen()
+
+if __name__ == '__main__':
+    PhrijjApp().run()
 
 
 # Subclass app below
