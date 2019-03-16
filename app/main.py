5ccfdb5d03c3618e3e0d61fb66a4ba4312720bf9
tobyjamez <tobyswjames@gmail.com>
2019-03-16 13:21:52 +0000

    Update to allow ENV variables as command line arguments

--- app/main.py
+++ app/main.py
@@ -1,7 +1,26 @@
+"""
+Main app loop for a kivy based application.
+
+@author="Toby James, Krishan Jethwa,
+Enrico Zammit Lonardelli, Davit Endeladze"
+"""
+
+import sys
+import os
+
+# Allow environment variables to be passed as command line arguments
+# In the form ENV_VARIABLE=value
+
+for env_variable in sys.argv[1:]:
+    try:
+        os.environ[env_variable.split("=")[0]] = env_variable.split("=")[1]
+    except(IndexError) as e:  # Passed value has no equals
+        pass
+
 import kivy
 import kivy.app as a
-kiv.require('1.10.1')
+kivy.require('1.10.1')
 
 
-class MainLoop(a.App):
-
+# Subclass app below
+# class MainLoop(a.App):
