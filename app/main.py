c0180e61bb8ecbb258335adbe50960683a96eb58
tobyjamez <tobyswjames@gmail.com>
2019-03-17 01:30:44 +0000

    Tidy up main.py

--- app/main.py
+++ app/main.py
@@ -31,8 +31,8 @@ kivy.require('1.10.1')
 
 
 class CustomTextInput(TextInput):
-    def set_opacity(self, opacity: float) -> type(None):
-        self.opacity = opacity
+    def toggle_opacity(self) -> type(None):
+        self.opacity = 1 - self.opacity
 
 
 class RootWidget(FloatLayout):
@@ -41,37 +41,21 @@ class RootWidget(FloatLayout):
     @staticmethod
     def button_trip(textinput, dropdown, widget):
         dropdown.open(widget)
-        textinput.set_opacity(1)
+        textinput.toggle_opacity()
 
     def __init__(self, **kwargs):
         # make sure we aren't overriding any important functionality
         super(RootWidget, self).__init__(**kwargs)
 
-        dropdown = DropDown()
-        enter_button = Button(text="Enter",
-                              size_hint=(1, .2),
-                              pos_hint={'center_x': 0.5, 'center_y': 0.07})
+        # Add widgets----------------------------------------------------------
 
-        # Add buttons to dropdown menu
-        for index in range(3):
-            dropdown_button = Button(text='Button %r' % index,
-                                     size_hint_y=None,
-                                     height=44)
-
-            dropdown.add_widget(dropdown_button)
-
-#        dropdown.bind(on_select=lambda instance, x: setattr(mainbutton,
-#                                                           'text',
-#                                                            x))
-
-        enter_button.bind(on_release=lambda widget: self.button_trip(textinput,
-                                                                     dropdown,
-                                                                     widget))
-
-        self.add_widget(enter_button, index=0)
+        # Add image ----------------------------------------------------
         self.add_widget(Image(source='Phrijj.png', size_hint=(1, 1),
                         pos_hint={'center_x': 0.5, 'center_y': 0.6}),
                         index=1)
+        # --------------------------------------------------------------
+
+        # Add textinput ------------------------------------------------
         textinput = CustomTextInput(hint_text="Search for a meal",
                                     multiline=False,
                                     size_hint_y=None,
@@ -81,6 +65,37 @@ class RootWidget(FloatLayout):
                                     opacity=0)
 
         self.add_widget(textinput)
+
+        # Add dropdown menu --------------------------------------------
+        dropdown = DropDown()
+
+        # Add buttons to dropdown menu
+        search_button = Button(text="Search",
+                               size_hint_y=None,
+                               height=44)
+
+        dropdown.add_widget(search_button)
+
+        show_button = Button(text="Show",
+                             size_hint_y=None,
+                             height=44)
+
+        dropdown.add_widget(show_button)
+
+        dropdown.bind(on_release=lambda x: textinput.toggle_opacity())
+        # --------------------------------------------------------------
+
+        # Add enter button ---------------------------------------------
+        enter_button = Button(text="Enter",
+                              size_hint=(1, .2),
+                              pos_hint={'center_x': 0.5, 'center_y': 0.07})
+
+        enter_button.bind(on_release=lambda widget: self.button_trip(textinput,
+                                                                     dropdown,
+                                                                     widget))
+
+        self.add_widget(enter_button, index=0)
+        # --------------------------------------------------------------
 
 
 class PhrijjApp(App):
