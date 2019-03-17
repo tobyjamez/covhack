88d3ff3b684cabf745d915f6495612dc741fd47e
tobyjamez <tobyswjames@gmail.com>
2019-03-17 11:04:27 +0000

    Multiple uses of each screen, home buttons and style guide compliance

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ from kivy.uix.dropdown import DropDown
 from kivy.uix.image import Image
 from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.graphics import Color, Rectangle 
+from kivy.graphics import Color, Rectangle
 
 from comm import search, add, show
 from productview import ProductView
@@ -36,12 +36,12 @@ kivy.require('1.10.1')
 class RootWidget(ScreenManager):
 
     def return_home(self, *args):
-        self.current="1"
+        self.current = "1"
 
-    def switch_screen(self, *args):
+    def switch_screen(self, button, *args):
         response_list = show().json()['data']
 
-        self.list_dropdown = DropDown(size_hint=(1,1))
+        self.list_dropdown = DropDown(size_hint=(1, 1))
 
         for item in response_list:
             self.list_dropdown.add_widget(ProductView(dict(name=item[0],
@@ -49,26 +49,32 @@ class RootWidget(ScreenManager):
                                                            provider=item[2]),
                                                       size_hint_y=None,
                                                       height=144,
-                                                      pos_hint={'center_x':0.5},
-                        color=(0, 0, 0, 1),
-                              background_color=(255, 255, 255, 255)))
+                                                      pos_hint={'center_x':
+                                                                0.5},
+                                                      color=(0, 0, 0, 1),
+                                                      background_color=(255,
+                                                                        255,
+                                                                        255,
+                                                                        255)))
 
-        buy_button = Button(text="Buy all", 
-                                                      size_hint_y=None,
-                                                      height=144,
-                                                      pos_hint={'center_x':0.5},
-                        color=(0, 0, 0, 1),
-                              background_color=(255, 255, 255, 255))
+        buy_button = Button(text="Home",
+                            size_hint_y=None,
+                            height=144,
+                            pos_hint={'center_x': 0.5},
+                            color=(0, 0, 0, 1),
+                            background_color=(255, 255, 255, 255))
 
         buy_button.bind(on_press=self.return_home)
 
         self.list_dropdown.add_widget(buy_button)
-
         self.current = "list"
-        self.list_screen.add_widget(self.list_dropdown)
+        try:
+            self.list_screen.add_widget(self.list_dropdown)
+        except Exception:
+            pass
         self.dropdown.dismiss()
 
-    def send_search(self, term, response_list, *args) -> type(None):
+    def send_search(self, term, response_list, button, *args) -> type(None):
 
         new_screen = "search"
 
@@ -77,21 +83,37 @@ class RootWidget(ScreenManager):
         if response_dict is None:
             return None
 
-
         else:
             response_list.append(response_dict)
             for item in response_list:
                 item_view = ProductView(item,
-                        color=(0, 0, 0, 1),
-                              background_color=(255, 255, 255, 255))
+                                        color=(0, 0, 0, 1),
+                                        background_color=(255, 255, 255, 255))
                 self.search_screen.add_widget(item_view)
+                response_list.pop(response_list.index(item))
 
             self.current = new_screen
             self.dropdown.dismiss()
+        self.search_screen.add_widget(button)
 
     def __init__(self, **kwargs):
         # make sure we aren't overriding any important functionality
         super().__init__(**kwargs)
+
+        self.home_button1 = Button(text="Home",
+                                   pos_hint={'center_x': 0.5,
+                                             'center_y': 0.95},
+                                   size_hint_y=None,
+                                   height=144)
+
+        self.home_button2 = Button(text="Home",
+                                   pos_hint={'center_x': 0.5,
+                                             'center_y': 0.95},
+                                   size_hint_y=None,
+                                   height=144)
+
+        self.home_button1.bind(on_press=self.return_home)
+        self.home_button2.bind(on_press=self.return_home)
 
         self.screen1 = Screen(name="1")
 
@@ -99,10 +121,10 @@ class RootWidget(ScreenManager):
         self.list_screen = Screen(name="list")
 
         # Add widgets----------------------------------------------------------
-        with self.screen1.canvas.before: 
-              Color(0,0,0,1) 
-              self.screen1.rect = Rectangle(size=self.screen1.size,
-                           pos=self.screen1.pos)
+        with self.screen1.canvas.before:
+            Color(0, 0, 0, 1)
+            self.screen1.rect = Rectangle(size=self.screen1.size,
+                                          pos=self.screen1.pos)
 
         def update_rect(instance, value):
             instance.rect.pos = instance.pos
@@ -110,10 +132,9 @@ class RootWidget(ScreenManager):
 
         # listen to size and position changes
         self.screen1.bind(pos=update_rect, size=update_rect)
-       
+
         # Add image ----------------------------------------------------
-        self.screen1.add_widget(Image(
-                             source='Phrijj.png', size_hint=(1, 1),
+        self.screen1.add_widget(Image(source='Phrijj.png', size_hint=(1, 1),
                                       pos_hint={'center_x': 0.5,
                                                 'center_y': 0.6}),
                                 index=1)
@@ -134,40 +155,40 @@ class RootWidget(ScreenManager):
 
         # Add buttons to dropdown menu
         search_button = Button(color=(0, 0, 0, 1),
-                              background_color=(255, 255, 255, 255),
-                              text="Search",
-                              size_hint_y=None,
-                              height=144)
+                               background_color=(255, 255, 255, 255),
+                               text="Search",
+                               size_hint_y=None,
+                               height=144)
 
         self.search_response_list = []
 
         search_button.bind(on_press=lambda widget:
                            self.send_search(self.textinput.text,
-                                            self.search_response_list))
+                                            self.search_response_list,
+                                            self.home_button1))
 
         self.dropdown.add_widget(search_button)
 
-        show_button = Button(color=(0, 0, 0, 1), 
+        show_button = Button(color=(0, 0, 0, 1),
                              background_color=(255, 255, 255, 255),
                              text="Show",
                              size_hint_y=None,
                              height=144)
 
-        show_button.bind(on_press=self.switch_screen)
+        show_button.bind(on_press=lambda x:
+                         self.switch_screen(self.home_button2))
 
         self.dropdown.add_widget(show_button)
 
         # --------------------------------------------------------------
 
         # Add enter button ---------------------------------------------
-        self.enter_button = Button(color=(0, 0, 0, 1), 
+        self.enter_button = Button(color=(0, 0, 0, 1),
                                    background_color=(255, 255, 255, 255),
                                    text="Start!",
                                    size_hint=(1, .2),
                                    pos_hint={'center_x': 0.5,
-                                             'center_y': 0.07},
-                                   
-                                    )
+                                             'center_y': 0.07})
 
         self.enter_button.bind(on_release=self.dropdown.open)
         self.screen1.add_widget(self.enter_button, index=0)
