25152e3ff910b3b37706f416c8ab0919a22fad20
Enrico <enrico.zammitl@gmail.com>
2019-03-17 01:09:05 +0000

    Added latest changes from extension

---
+++ chrome/popup.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+
+    chrome.tabs.getSelected(null, function(tab) {
+      d = document;
+      
+    });
+  }, false);
+}, false);
