98590f7343ea8cfd52cd6f437a546f7500c3241d
Enrico <enrico.zammitl@gmail.com>
2019-03-17 03:51:18 +0000

    API changes

--- server/lowestprice.php
+++ server/lowestprice.php
@@ -4,7 +4,6 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 include_once ('db.php');
 $data = json_decode($data);
